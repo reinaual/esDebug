f370e1beebc6d84e59691d9dbb14ff53380695e6
Alexander Reinauer <st144434@stud.uni-stuttgart.de>
2018-09-27 21:23:49 +0200

    reverted iccTypes back to iccdata

--- src/core/electrostatics_magnetostatics/icc.hpp
+++ src/core/electrostatics_magnetostatics/icc.hpp
@@ -67,6 +67,7 @@
 struct iccp3m_data_struct {
   int largestID = 0;
   NewParticle reducedPart;
+  std::vector<iccShape *> iccTypes;
   std::list<std::vector<int>> trackList;
   std::queue<std::vector<NewParticle>> newParticleData;
   std::vector<double> iccCharges;
@@ -90,7 +91,6 @@ struct iccp3m_struct {
   int first_id = 0;
   double maxCharge = 0.;
   double minCharge = 0.;
-  std::vector<iccShape *> iccTypes;
 
   template <typename Archive>
   void serialize(Archive &ar, long int /* version */) {
